a5427bbee226f199ee0450da96f5bba0c48c59f8
Juan Montoya <montoyavasquezj429@mail.com>
2023-04-09 21:08:07 -0500

    se resuelve el ejercicio6 sobre mostar el indice de una lista

---
+++ ejercicio6.py
@@ -0,0 +1,6 @@
+numeros=1,2,3,4,5,6,7,8,9,10
+
+numero_Ingresado=int(input("ingrese un indice a mostar: "))
+
+
+print(f"el indice {numero_Ingresado} que contiene el numero {numeros[numero_Ingresado]}")
