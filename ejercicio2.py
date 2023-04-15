f69552f2de67734217096e288f5d1f6c00135c42
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-04-15 10:56:16 -0500

    se termino el ejercicio2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,7 @@
 numeros = (2,1,3,5,10,7,9,4,6,8)
 
-for e in range(len(numeros)):
-  if numeros[e] <= numeros[e]:
-    mayor = numeros[e]
-    print(mayor)
+mayor = max(numeros)
+minimo = min(numeros)
+
+print(mayor)
+print(minimo)
